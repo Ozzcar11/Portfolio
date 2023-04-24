b01350358cbba3780f4d9b2278b39552759cf84b
Atav <ozzcar11@gmail.com>
2023-04-24 23:53:08 +0300

    feat: add fonts

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,17 @@
-<script setup>
-
-</script>
-
+<script setup></script>
 
 <template>
-   <div>
-      
-   </div>
-   <Loader />
+  <div>
+    <header class="header"></header>
+    <h1 class="title">Даниялов Атав</h1>
+  </div>
+  <Loader />
 </template>
 
-
-<style lang="scss"></style>
+<style lang="scss">
+.title {
+  font-family: Montserrat;
+  font-weight: 300;
+  font-style: italic;
+}
+</style>
